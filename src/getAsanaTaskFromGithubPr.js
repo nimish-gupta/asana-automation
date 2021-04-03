46a6fca413cb31b24e20760638157da1a0fe997f
nimish-gupta <nimish@loancrate.com>
2021-04-03 19:35:29 +0530

    fetched the asana ids from the github pr

--- src/getAsanaTaskFromGithubPr.js
+++ src/getAsanaTaskFromGithubPr.js
@@ -1,10 +1,17 @@
 const enquirer = require('enquirer');
+const ParseGithubUrl = require('parse-github-url');
+const { Octokit } = require('@octokit/rest');
+const getUrls = require('get-urls');
 
 const fetchAndSaveToken = require('./fetchAndSaveToken');
 const getToken = require('./getToken');
 const log = require('./log');
 
 const GITHUB_TOKEN = 'GITHUB_PERSONAL_TOKEN';
+const ASANA_DOMAIN = 'app.asana.com';
+const URL_SEPARATOR = '/';
+
+const isLinkForFullScreen = (asanaLinkParts) => asanaLinkParts[0] === 'f';
 
 async function main(githubPR) {
 	let githubToken = getToken(GITHUB_TOKEN);
@@ -18,6 +25,10 @@ async function main(githubPR) {
 		throw new Error("Couldn't found the github token");
 	}
 
+	const octokit = new Octokit({
+		auth: githubToken,
+	});
+
 	const { prLink } =
 		githubPR === true
 			? await enquirer.prompt({
@@ -27,6 +38,31 @@ async function main(githubPR) {
 			  })
 			: { prLink: githubPR };
 	log('Successfully got the pr link', prLink);
+
+	const parsedUrl = ParseGithubUrl(prLink);
+	log('Successfully parsed the github url', parsedUrl);
+
+	const pullRequest = await octokit.pulls.get({
+		owner: parsedUrl.owner,
+		repo: parsedUrl.name,
+		pull_number: parsedUrl.filepath,
+	});
+	log('Successfully fetched the pull request', pullRequest.url);
+
+	const urls = Array.from(getUrls(pullRequest.data.body));
+	log('Successfully fetched the urls from pull request', urls);
+
+	const asanaLinks = urls.filter((url) => url.includes(ASANA_DOMAIN));
+	if (asanaLinks.length === 0) {
+		return 'No asana link found in the PR';
+	}
+	log('Successfully filtered asana links', asanaLinks);
+
+	const asanaTaskIds = asanaLinks.map((link) => {
+		const linkParts = link.split(URL_SEPARATOR).reverse();
+		return linkParts[isLinkForFullScreen(linkParts) ? 1 : 0];
+	});
+	log('Successfully fetched the task ids', asanaTaskIds);
 }
 
 module.exports = main;
